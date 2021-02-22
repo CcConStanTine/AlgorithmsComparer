25b3ba64407c4e55d499ba2d88046026c93d9ba9
Paweł Karpiel <pawel215999@gmail.com>
2021-02-22 18:27:38 +0100

    Migrating the project to GitHub

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Projekt_v2._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox_algorithms = new System.Windows.Forms.CheckedListBox();
+            this.comboBox_dataSetSize = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox_dataPattern = new System.Windows.Forms.ComboBox();
+            this.checkBox_Multithreaded = new System.Windows.Forms.CheckBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(71, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(954, 78);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sorting algorithms comparer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(268, 379);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(535, 55);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Choose the algorithms";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(440, 725);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 100);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Sort!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox_algorithms
+            // 
+            this.checkBox_algorithms.BackColor = System.Drawing.SystemColors.Info;
+            this.checkBox_algorithms.Items.AddRange(new object[] {
+            "Selection sort",
+            "Bubble sort",
+            "Insertion sort",
+            "Merge sort",
+            "Quick sort",
+            "Heap sort"});
+            this.checkBox_algorithms.Location = new System.Drawing.Point(178, 450);
+            this.checkBox_algorithms.Name = "checkBox_algorithms";
+            this.checkBox_algorithms.Size = new System.Drawing.Size(738, 214);
+            this.checkBox_algorithms.TabIndex = 4;
+            // 
+            // comboBox_dataSetSize
+            // 
+            this.comboBox_dataSetSize.FormattingEnabled = true;
+            this.comboBox_dataSetSize.Items.AddRange(new object[] {
+            "100",
+            "1000",
+            "2500",
+            "5000",
+            "10000",
+            "20000",
+            "50000",
+            "100000",
+            "200000",
+            "500000",
+            "1000000"});
+            this.comboBox_dataSetSize.Location = new System.Drawing.Point(213, 318);
+            this.comboBox_dataSetSize.Name = "comboBox_dataSetSize";
+            this.comboBox_dataSetSize.Size = new System.Drawing.Size(200, 39);
+            this.comboBox_dataSetSize.TabIndex = 5;
+            this.comboBox_dataSetSize.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(116, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(399, 46);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Choose dataset size";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(612, 258);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(400, 46);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Choose data pattern";
+            // 
+            // comboBox_dataPattern
+            // 
+            this.comboBox_dataPattern.FormattingEnabled = true;
+            this.comboBox_dataPattern.Items.AddRange(new object[] {
+            "Rising values",
+            "Decreasing values",
+            "Random values"});
+            this.comboBox_dataPattern.Location = new System.Drawing.Point(717, 318);
+            this.comboBox_dataPattern.Name = "comboBox_dataPattern";
+            this.comboBox_dataPattern.Size = new System.Drawing.Size(200, 39);
+            this.comboBox_dataPattern.TabIndex = 8;
+            this.comboBox_dataPattern.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // checkBox_Multithreaded
+            // 
+            this.checkBox_Multithreaded.AutoSize = true;
+            this.checkBox_Multithreaded.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.checkBox_Multithreaded.Location = new System.Drawing.Point(66, 153);
+            this.checkBox_Multithreaded.Name = "checkBox_Multithreaded";
+            this.checkBox_Multithreaded.Size = new System.Drawing.Size(927, 50);
+            this.checkBox_Multithreaded.TabIndex = 9;
+            this.checkBox_Multithreaded.Text = "Choose if program should use multiple threads";
+            this.checkBox_Multithreaded.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1168, 912);
+            this.Controls.Add(this.checkBox_Multithreaded);
+            this.Controls.Add(this.comboBox_dataPattern);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox_dataSetSize);
+            this.Controls.Add(this.checkBox_algorithms);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Sorting algorithms comparer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox checkBox_algorithms;
+        private System.Windows.Forms.ComboBox comboBox_dataSetSize;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox_dataPattern;
+        private System.Windows.Forms.CheckBox checkBox_Multithreaded;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,403 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Projekt_v2._0
+{
+    public partial class Form1 : Form
+    {
+        List<int> indexes;
+        List<int> dataSet;
+        Stopwatch stopwatch1 = new Stopwatch();
+        Stopwatch stopwatch2 = new Stopwatch();
+        List<ManualResetEvent> listOfSyncEvents;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            backgroundWorker1.RunWorkerCompleted += backgroundWorker1_RunWorkerCompleted;
+            backgroundWorker1.DoWork += backgroundWorker1_DoWork;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            sortUsingMultithreading();
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Thread newThread = new Thread(() =>
+            {
+                foreach(ManualResetEvent syncEvent in listOfSyncEvents)
+                {
+                    syncEvent.WaitOne();
+                }
+                stopwatch1.Stop();
+                MessageBox.Show("Multithreaded sorting took: " + stopwatch2.ElapsedMilliseconds + " milliseconds. ");
+            });
+            newThread.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            indexes = new List<int>();
+            foreach (int index in checkBox_algorithms.CheckedIndices)
+            {
+                indexes.Add(index);
+            }
+            if (indexes.Capacity == 0)
+                MessageBox.Show("Choose at least one algorithm!");
+            else if (comboBox_dataSetSize.SelectedItem == null)
+                MessageBox.Show("Select data set size!");
+            else if (comboBox_dataSetSize.SelectedItem == null)
+                MessageBox.Show("Select data pattern!");
+            else if (!checkBox_Multithreaded.Checked)
+            {
+                stopwatch1.Reset();
+                stopwatch1.Start();
+                sortWithoutUsingMultithreading();
+                stopwatch1.Stop();
+                MessageBox.Show("Non-multithreaded soring took: " + stopwatch1.ElapsedMilliseconds + " milliseconds. ");
+            }
+            else
+            {
+                stopwatch2.Reset();
+                stopwatch2.Start();
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void sortUsingMultithreading()
+        {
+            listOfSyncEvents  = new List<ManualResetEvent>(6);
+            for (int i=0; i<6; i++)
+            {
+                listOfSyncEvents.Add(new ManualResetEvent(true));
+            }
+            if (indexes.Contains(0))
+             {
+                listOfSyncEvents[0] = new ManualResetEvent(false);
+                List<int> sortedDataSet = new List<int>(dataSet);
+                Thread newThread = new Thread(() => {
+                    selectionSort(sortedDataSet);
+                    listOfSyncEvents[0].Set();
+                });
+                newThread.Start();
+            }
+            if (indexes.Contains(1))
+            {
+                listOfSyncEvents[1] = new ManualResetEvent(false);
+                List<int> sortedDataSet = new List<int>(dataSet);
+                Thread newThread = new Thread(() => {
+                    bubbleSort(sortedDataSet);
+                    listOfSyncEvents[1].Set();
+                });
+                newThread.Start();
+
+            }
+            if (indexes.Contains(2))
+            {
+                listOfSyncEvents[2] = new ManualResetEvent(false);
+                List<int> sortedDataSet = new List<int>(dataSet);
+                Thread newThread = new Thread(() => {
+                    insertionSort(sortedDataSet);
+                    listOfSyncEvents[2].Set();
+                });
+                newThread.Start();
+            }
+            if (indexes.Contains(3))
+            {
+                listOfSyncEvents[3] = new ManualResetEvent(false);
+                List<int> sortedDataSet = new List<int>(dataSet);
+                Thread newThread = new Thread(() => {
+                    mergeSort(sortedDataSet, 0, sortedDataSet.Capacity - 1);
+                    listOfSyncEvents[3].Set();
+                });
+                newThread.Start();
+            }
+            if (indexes.Contains(4))
+            {
+                listOfSyncEvents[4] = new ManualResetEvent(false);
+                List<int> sortedDataSet = new List<int>(dataSet);
+                Thread newThread = new Thread(() => {
+                    quickSort(sortedDataSet, 0, sortedDataSet.Capacity - 1);
+                    listOfSyncEvents[4].Set();
+                });
+                newThread.Start();
+            }
+            if (indexes.Contains(5))
+            {
+                listOfSyncEvents[5] = new ManualResetEvent(false);
+                List<int> sortedDataSet = new List<int>(dataSet);
+                Thread newThread = new Thread(() => {
+                    heapSort(sortedDataSet, sortedDataSet.Capacity - 1);
+                    listOfSyncEvents[5].Set();
+                });
+                newThread.Start();
+            }
+        }
+        private void sortWithoutUsingMultithreading()
+        {
+            if (indexes.Contains(0))
+            {
+                List<int> sortedDataSet = new List<int>(dataSet);
+                selectionSort(sortedDataSet);
+            }
+            if (indexes.Contains(1))
+            {
+                List<int> sortedDataSet = new List<int>(dataSet);
+                bubbleSort(sortedDataSet);
+            }
+            if (indexes.Contains(2))
+            {
+                List<int> sortedDataSet = new List<int>(dataSet);
+                insertionSort(sortedDataSet);
+            }
+            if (indexes.Contains(3))
+            {
+                List<int> sortedDataSet = new List<int>(dataSet);
+                mergeSort(sortedDataSet, 0, sortedDataSet.Capacity - 1);
+            }
+            if (indexes.Contains(4))
+            {
+                List<int> sortedDataSet = new List<int>(dataSet);
+                quickSort(sortedDataSet, 0, sortedDataSet.Capacity - 1);
+            }
+            if (indexes.Contains(5))
+            {
+                List<int> sortedDataSet = new List<int>(dataSet);
+                heapSort(sortedDataSet, sortedDataSet.Capacity - 1);
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox_dataPattern.SelectedItem != null)
+            {
+                int dataPattern = comboBox_dataPattern.SelectedIndex;
+                int selectedDataSetSize = Convert.ToInt32(comboBox_dataSetSize.SelectedItem);
+                dataSet = new List<int>(selectedDataSetSize);
+                Thread newThread = new Thread(() => fillListWithData(dataPattern, selectedDataSetSize));
+                newThread.Start();
+            }
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox_dataSetSize.SelectedItem != null)
+            {
+                int dataPattern = comboBox_dataPattern.SelectedIndex;
+                int selectedDataSetSize = Convert.ToInt32(comboBox_dataSetSize.SelectedItem);
+                dataSet = new List<int>(selectedDataSetSize);
+                Thread newThread = new Thread(() => fillListWithData(dataPattern, selectedDataSetSize));
+                newThread.Start();
+            }
+        }
+        private void fillListWithData(int dataPattern, int selectedDataSetSize)
+        {
+            switch(dataPattern)
+            {
+                case 0:
+                    {
+                        for (int i = 0; i < selectedDataSetSize; i++)
+                        {
+                            dataSet.Add(i);
+                        }
+                    } break;
+                case 1:
+                    {
+                        for (int i = selectedDataSetSize-1; i >= 0; i--)
+                        {
+                            dataSet.Add(i);
+                        }
+                    }
+                    break;
+                case 2:
+                    {
+                        Random random = new Random();
+                        for (int i = 0; i < selectedDataSetSize; i++)
+                        {
+                            dataSet.Add(random.Next(0, selectedDataSetSize));
+                        }
+                    }
+                    break;
+            }
+        }
+        private void selectionSort(List<int> list)
+        {
+            int temp, smallest;
+            for (int i = 0; i < list.Capacity - 1; i++)
+            {
+                smallest = i;
+                for (int j = i + 1; j < list.Capacity; j++)
+                {
+                    if (list[j] < list[smallest])
+                    {
+                        smallest = j;
+                    }
+                }
+                temp = list[smallest];
+                list[smallest] = list[i];
+                list[i] = temp;
+            }
+        }
+        private void bubbleSort(List<int> list)
+        {
+            int temp;
+            for (int j = 0; j <= list.Capacity - 2; j++)
+            {
+                for (int i = 0; i <= list.Capacity - 2; i++)
+                {
+                    if (list[i] > list[i + 1])
+                    {
+                        temp = list[i + 1];
+                        list[i + 1] = list[i];
+                        list[i] = temp;
+                    }
+                }
+            }
+        }
+        private void insertionSort(List<int> list)
+        {
+            int i, j, val, flag;
+            for (i = 1; i < list.Capacity; i++)
+            {
+                val = list[i];
+                flag = 0;
+                for (j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (val < list[j])
+                    {
+                        list[j + 1] = list[j];
+                        j--;
+                        list[j + 1] = val;
+                    }
+                    else flag = 1;
+                }
+            }
+        }
+        private void mergeSort(List<int> list, int p, int r)
+        {
+            if (p < r)
+            {
+                int q = (p + r) / 2;
+                mergeSort(list, p, q);
+                mergeSort(list, q + 1, r);
+                merge(list, p, q, r);
+            }
+        }
+        static public void merge(List<int> list, int p, int q, int r)
+        {
+            int i, j, k;
+            int n1 = q - p + 1;
+            int n2 = r - q;
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+            for (i = 0; i < n1; i++)
+            {
+                L[i] = list[p + i];
+            }
+            for (j = 0; j < n2; j++)
+            {
+                R[j] = list[q + 1 + j];
+            }
+            i = 0;
+            j = 0;
+            k = p;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    list[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    list[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < n1)
+            {
+                list[k] = L[i];
+                i++;
+                k++;
+            }
+            while (j < n2)
+            {
+                list[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+        private void quickSort(List<int> list, int start, int end)
+        {
+            int i;
+            if (start < end)
+            {
+                i = partition(list, start, end);
+
+                quickSort(list, start, i - 1);
+                quickSort(list, i + 1, end);
+            }
+        }
+        private int partition(List<int> list, int start, int end)
+        {
+            int temp;
+            int p = list[end];
+            int i = start - 1;
+
+            for (int j = start; j <= end - 1; j++)
+            {
+                if (list[j] <= p)
+                {
+                    i++;
+                    temp = list[i];
+                    list[i] = list[j];
+                    list[j] = temp;
+                }
+            }
+
+            temp = list[i + 1];
+            list[i + 1] = list[end];
+            list[end] = temp;
+            return i + 1;
+        }
+        private void heapSort(List<int> list, int n)
+        {
+            for (int i = n / 2 - 1; i >= 0; i--)
+                heapify(list, n, i);
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int temp = list[0];
+                list[0] = list[i];
+                list[i] = temp;
+                heapify(list, i, 0);
+            }
+        }
+        private void heapify(List<int> list, int n, int i)
+        {
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            if (left < n && list[left] > list[largest])
+                largest = left;
+            if (right < n && list[right] > list[largest])
+                largest = right;
+            if (largest != i)
+            {
+                int swap = list[i];
+                list[i] = list[largest];
+                list[largest] = swap;
+                heapify(list, n, largest);
+            }
+        }
+
+    }
+}
